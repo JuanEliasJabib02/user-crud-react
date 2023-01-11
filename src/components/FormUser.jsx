6db82e17ae9f22895fa3da0ee0d36c29b047eb21
JuanEliasJabib02 <juaneliasjabib02@gmail.com>
2023-01-11 12:40:43 -0500

    form and card css finished

--- src/components/FormUser.jsx
+++ src/components/FormUser.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import { useForm } from "react-hook-form"
-
+import "./styles/formUser.css"
 
 
 const FormUser = ({ createUser, updateInfo,updateUserById }) => {
@@ -32,19 +32,39 @@ const FormUser = ({ createUser, updateInfo,updateUserById }) => {
 
 
   return (
-    <form onSubmit={handleSubmit(submit)} action="">
-      <h2>Create User</h2>
-      <label htmlFor="email">Email</label>
-      <input id="email" type="text"{...register("email")} />
-      <label htmlFor="password">Password</label>
-      <input id="password" type="text" {...register("password")}/>
-      <label htmlFor="first_name">First Name</label>
-      <input id="first_name" type="text" {...register("first_name")} />
-      <label htmlFor="last_name">Last Name</label>
-      <input id="last_name" type="text" {...register("last_name")} />
-      <label htmlFor="birthday">Birthday</label>
-      <input id="birthday" type="date" {...register("birthday")} />
-      <button>SUBMIT</button> 
+    <form className='form' onSubmit={handleSubmit(submit)} action="">
+      <h2 className='form__title'>Create User</h2>
+      <div className="form__div">
+        <label className='form__label' htmlFor="email">
+          Email
+        </label>
+        <input className='form__input' id="email" type="text"{...register("email")} />
+      </div>
+      <div className="form__div">
+        <label className='form__label' htmlFor="password">
+          Password
+        </label>
+        <input  className="form__input"id="password" type="text" {...register("password")} />
+      </div>
+      <div className="form__div">
+        <label  class ="form__label"htmlFor="first_name">
+          First Name
+        </label>
+        <input className='form__input' id="first_name" type="text" {...register("first_name")} />
+      </div>
+      <div className="form__div">
+        <label className='form__label' htmlFor="last_name">
+          Last Name
+        </label>
+        <input className='form__input' id="last_name" type="text" {...register("last_name")} />
+      </div>
+      <div className="form__div">
+        <label className='form__label' htmlFor="birthday">
+          Birthday
+        </label>
+        <input className='form__input' id="birthday" type="date" {...register("birthday")} />
+      </div>
+      <button className='form__submit-btn'>Submit</button> 
    
     </form>
   )
--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import axios from "axios"
+import "./styles/userCard.css"
 
 const UserCard = ({ user, getAllUsers, updateUserById,setUpdateInfo }) => {
   
@@ -17,16 +18,20 @@ const UserCard = ({ user, getAllUsers, updateUserById,setUpdateInfo }) => {
 
   return (
     <article className="user-card">
-      <header>
+      <header className='user-card__header'>
         <h3 className="user-card__name">
           {user.first_name} {user.last_name}
         </h3>
       </header>
-      <ul>
-        <li><span>Email: </span>{user.email}</li>
-        <li><span>Birthday:</span>{user.birthday}</li>
+      <ul className='user-card__list'>
+        <li className='user-card__list-item'>
+          <span>Email </span><p>{user.email}</p>
+        </li>
+        <li className='user-card__list-item'>
+          <span>Birthday</span><p>{user.birthday}</p>
+        </li>
       </ul>
-      <footer>
+      <footer className='user-card__footer'>
         <button onClick={handleUpdate} className="user-card__edit">
           <i className="fa-solid fa-pen-to-square"></i>
         </button>
